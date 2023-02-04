2bd49a666137c0c9332232b4af90d87217ba0cc6
Francois Chollet <francois.chollet@gmail.com>
2023-02-03 16:34:07 -0800

    Minor fixes

--- namex/convert.py
+++ namex/convert.py
@@ -60,7 +60,7 @@ def convert_codebase(package, code_directory="src"):
         raise ValueError(f"No directory named '{package}'.")
     os.rename(package, code_directory)
     os.mkdir(package)
-    shutil.copytree(code_directory, os.path.join(package, code_directory))
+    shutil.move(code_directory, os.path.join(package, code_directory))
     rewrite_python_file_imports(
         target_dir=package, root_name=package, offset_name="src", verbose=True
     )
