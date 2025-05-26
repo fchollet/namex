e769dba3841bcbf7e23f13517a6fe6102a199a88
Francois Chollet <francois.chollet@gmail.com>
2025-05-26 16:17:54 -0700

    update version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="namex",
-    version="0.0.9",
+    version="0.1.0",
     description=(
         "A simple utility to separate "
         "the implementation of your Python package "
